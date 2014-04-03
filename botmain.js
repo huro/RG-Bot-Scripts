a7cedd0aeed14b373d16f3f32334071f42f9bb00
huro <toonchannel@gmail.com>
2014-04-03 10:54:49 -0400

    Update botmain.js

--- botmain.js
+++ botmain.js
@@ -1,7 +1,7 @@
 // Huro/Hearrk
 /* Do not attempt to pass as own work.
 /* Before changing anything please ask for permission.
-/* Copyright OmniscientLegacy/Huro/Heark
+/* Copyright Ⓒ OmniscientLegacy/Huro/Heark
 /* Special Thanks:
 /* Silone1, Leon Powalski, NeonLights, Alice(Cirno), Lutra
 */
